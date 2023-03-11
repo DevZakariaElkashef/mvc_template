b684e01faeb0a4e70e60f2375a61ba5c575c5f3e
DevZakariaElkashef <dev.zakariaelkashef@gmail.com>
2023-03-11 14:33:09 +0200

    adding twig template

--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -22,6 +22,8 @@ class ComposerAutoloaderInita59a8fd9dabd765b9ca5b41a69d9cbf8
             return self::$loader;
         }
 
+        require __DIR__ . '/platform_check.php';
+
         spl_autoload_register(array('ComposerAutoloaderInita59a8fd9dabd765b9ca5b41a69d9cbf8', 'loadClassLoader'), true, true);
         self::$loader = $loader = new \Composer\Autoload\ClassLoader(\dirname(__DIR__));
         spl_autoload_unregister(array('ComposerAutoloaderInita59a8fd9dabd765b9ca5b41a69d9cbf8', 'loadClassLoader'));
@@ -30,6 +32,18 @@ class ComposerAutoloaderInita59a8fd9dabd765b9ca5b41a69d9cbf8
         call_user_func(\Composer\Autoload\ComposerStaticInita59a8fd9dabd765b9ca5b41a69d9cbf8::getInitializer($loader));
 
         $loader->register(true);
+
+        $filesToLoad = \Composer\Autoload\ComposerStaticInita59a8fd9dabd765b9ca5b41a69d9cbf8::$files;
+        $requireFile = \Closure::bind(static function ($fileIdentifier, $file) {
+            if (empty($GLOBALS['__composer_autoload_files'][$fileIdentifier])) {
+                $GLOBALS['__composer_autoload_files'][$fileIdentifier] = true;
+
+                require $file;
+            }
+        }, null, null);
+        foreach ($filesToLoad as $fileIdentifier => $file) {
+            $requireFile($fileIdentifier, $file);
+        }
 
         return $loader;
     }
